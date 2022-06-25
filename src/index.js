b44f792c92aede06880e08f4f01bbb298c685040
pranjal1006 <60665945+pranjal1006@users.noreply.github.com>
2022-06-25 21:53:51 +0530

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Join Elite Warriors!</title>
+    <link rel="stylesheet" href="styles.css">
+  </head>
+  <body>
+	<h1>In case the link is not working, search for the clan by tag and send a join request directly, clan is on Invite Only mode.</h1>
+	<h1>Clan Tag: #Q02UV0C0</h1>
+    <input type="button" onclick="location.href='https://link.clashroyale.com/invite/clan/en?tag=Q02UV0C0&token=hfrh68x3&platform=android';" value="Join Elite Warriors!" />
+  </body>
+</html>
